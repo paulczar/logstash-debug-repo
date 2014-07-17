2a777583c116fff17a6741fba40b3f6032e20edd
paulczar <paul@paulcz.net>
2014-07-17 08:48:20 -0500

    0.9.2 works

--- metadata.rb
+++ metadata.rb
@@ -7,4 +7,4 @@ description      'Installs/Configures loltest'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-depends 'logstash', '0.9.1'
+depends 'logstash', '0.9.2'
